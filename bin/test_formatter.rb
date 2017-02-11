f9d8866cb0ad0d08af694c7a2571fd972b95a2b7
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-02-11 22:01:34 +0200

    Removed the loading of path

--- bin/test_formatter.rb
+++ bin/test_formatter.rb
@@ -1,13 +1,5 @@
 # File: bin/test_formatter.rb
 #
-# Let's put the project 'lib' folder into the load path
-# Note that "File.expand_path('..', __FILE__)" returns the
-# folder this file ('test_formatter.rb') belongs to.
-# Also, the File.join() is used to join strings to build a file path in a system
-# independent way, hence it works both for Linux and Windows machines
-#
-$LOAD_PATH.unshift(File.join(File.expand_path('..', __FILE__), '..', 'lib'))
-
 require 'tcb_letter_spacing'
 
 formatter = TcbLetterSpacing::Formatter.new('foo')
